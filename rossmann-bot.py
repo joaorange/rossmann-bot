ceb8b26655754a926f631bf0fc58c2fab64f6138
João Victor <86979717+joaorange@users.noreply.github.com>
2023-06-01 16:49:28 -0300

    Update rossmann-bot.py

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -61,13 +61,13 @@ def parse_message(message):
     store_id = store_id.replace('/','')
     
     if store_id == 'start':
-        store_id = 'start':
-    else:        
+        store_id = "start"
+    else:    
         try:
             store_id = int(store_id)
         except ValueError:
             store_id = 'error'
-
+        
     return chat_id, store_id
 
 # API initialize
